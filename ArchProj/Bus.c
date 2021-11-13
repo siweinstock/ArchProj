89bd92ec6edcf091a38eed4f36a824e2d4a6350b
Alon Beinhorn <alonbeinhorn@mail.tau.ac.il>
2021-11-13 20:33:49 +0200

    Starting to work on the bus code

--- ArchProj/Bus.c
+++ ArchProj/Bus.c
@@ -7,16 +7,17 @@
  * The rest 12 bits of the adress is tag (the address is 20 bits).
  */
 
-#define BLOCKS_IN_CACH    64
+#define BLOCKS_IN_CACHE    64
 #define WORDS_IN_BLOCK	  4
-#define TSRAM_SIZE        64
 #define MAIN_MEM_SIZE     1048576
+
+int main_memory[MAIN_MEM_SIZE];
 
 
 typedef struct DSRAM {
 	int core_index;
 	// CORE* core;
-	int sram[BLOCKS_IN_CACH][WORDS_IN_BLOCK];
+	int sram[BLOCKS_IN_CACHE][WORDS_IN_BLOCK];
 
 } DSRAM;
 
@@ -24,46 +25,43 @@ typedef struct DSRAM {
 typedef struct TSRAM {
 	int core_index;
 	// CORE* core;
-	int tags[TSRAM_SIZE];
-	int MESI[TSRAM_SIZE];
+	int tags[BLOCKS_IN_CACHE];
+	int MESI[BLOCKS_IN_CACHE];
 } TSRAM;
 
 
-DSRAM* dsram0, * dsram1, * dsram2, * dsram3;
+DSRAM* dsrams_array[4];
 
-TSRAM* tsram0, * tsram1, * tsram2, * tsram3;
+TSRAM* tsrams_array[4];
 
 
 int priorities[4] = { 0, 1, 2, 3 };
 int requests[4] = { 0, 0, 0, 0 };
 
-char bus_origid;
-char bus_cmd;
-char bus_addr;
-char bus_data;
-char bus_shared;
+char bus_origid, bus_cmd, bus_addr, bus_data, bus_shared;
 
 
 void init() {
-	dsram0 = calloc(1, sizeof(DSRAM));
-	dsram0->core_index = 0;
-	dsram1 = calloc(1, sizeof(DSRAM));
-	dsram1->core_index = 1;
-	dsram2 = calloc(1, sizeof(DSRAM));
-	dsram2->core_index = 2;
-	dsram3 = calloc(1, sizeof(DSRAM));
-	dsram3->core_index = 3;
+	dsrams_array[0] = calloc(1, sizeof(DSRAM));
+	dsrams_array[0]->core_index = 0;
+	dsrams_array[1] = calloc(1, sizeof(DSRAM));
+	dsrams_array[1]->core_index = 1;
+	dsrams_array[2] = calloc(1, sizeof(DSRAM));
+	dsrams_array[2]->core_index = 2;
+	dsrams_array[3] = calloc(1, sizeof(DSRAM));
+	dsrams_array[3]->core_index = 3;
 	
-	tsram0 = calloc(1, sizeof(TSRAM));
-	tsram0->core_index = 0;
-	tsram1 = calloc(1, sizeof(TSRAM));
-	tsram1->core_index = 1;
-	tsram2 = calloc(1, sizeof(TSRAM));
-	tsram2->core_index = 2;
-	tsram3 = calloc(1, sizeof(TSRAM));
-	tsram3->core_index = 3;
+	tsrams_array[0] = calloc(1, sizeof(TSRAM));
+	tsrams_array[0]->core_index = 0;
+	tsrams_array[1] = calloc(1, sizeof(TSRAM));
+	tsrams_array[1]->core_index = 1;
+	tsrams_array[2] = calloc(1, sizeof(TSRAM));
+	tsrams_array[2]->core_index = 2;
+	tsrams_array[3] = calloc(1, sizeof(TSRAM));
+	tsrams_array[3]->core_index = 3;
 }
 
+//****************** Code for the round robin implementation ************************************************************
 void core_used_bus(int* priorities_arr, int core_num) {
 	int i, j, temp;
 
@@ -90,5 +88,59 @@ int choose_core(int* priorities, int* requests) {
 		}
 	}
 	return core_num;
+}
+//*************************************************************************************************************************
+
+void BusRd() {
+
+}
+
+void BusRdX() {
+
+}
+
+
+void PrRd(int core_index, int addr, int* data) {
+	int offset = addr & 0x3;
+	int index = (addr >> 2) & 0x3F;
+	int tag = (addr >> 8) & 0xFFF;
+	TSRAM* tsram = tsrams_array[core_index];
+	DSRAM* dsram = dsrams_array[core_index];
+
+	if (tag == tsram->tags[index] && tsram->MESI[index]) { // If there is cache hit and the content is valid we just take the content
+		*data = dsram->sram[index][offset];
+	}
+	else if(tag == tsram->tags[index]){
+		BusRd();
+	}
+	else if (tsram->MESI[index] = 1 || tsram->MESI[index] = 0){
+		BusRd();
+	}
+}
+
+
+void PrWr(int core_index, int addr, int* data) {
+	int offset = addr & 0x3;
+	int index = (addr >> 2) & 0x3F;
+	int tag = (addr >> 8) & 0xFFF;
+	TSRAM* tsram = tsrams_array[core_index];
+	DSRAM* dsram = dsrams_array[core_index];
+
+	if (tag == tsram->tags[index] && tsram->MESI[index] == 3) { // If there is cache hit and the state is M we just modify the content
+		dsram->sram[index][offset] = *data;
+	}
+	else if (tag == tsram->tags[index] && tsram->MESI[index] == 2) { // If there is cache hit and the state is E we modify the content and go to state M
+		dsram->sram[index][offset] = *data;
+		tsram->MESI[index] = 3;
+	}
+	else if (tag == tsram->tags[index] && tsram->MESI[index] == 1) { // If there is cache hit and the state is S same as above but activate BusRdX
+		dsram->sram[index][offset] = *data;
+		tsram->MESI[index] = 3;
+		BusRdX();
+	}
+	else if (tag == tsram->tags[index]) { // If there is cache hit and the state is S same as above but activate BusRdX
+		tsram->MESI[index] = 3;
+		BusRdX();
+	}
 }
 
